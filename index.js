bdd644129e9d2b62cd3bb1e00b1c688b7d7a0169
Ian Walter <public@iankwalter.com>
2019-03-19 15:59:32 -0400

    Fixing #14

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ import execa from 'execa'
 export default function execaHelper (cli, throwOnStdErr = true) {
   return async function withCli (t, run) {
     await run(t, async (...params) => {
-      const result = await execa(cli, params)
+      const lastParam = params[params.length - 1]
+      const options = typeof lastParam === 'object' ? params.pop() : {}
+      const result = await execa(cli, params, options)
 
       // Throw an error if stderr has content so that the test fails.
       if (throwOnStdErr && result.stderr) {
--- test/test.js
+++ test/test.js
@@ -1,8 +1,10 @@
+import { resolve } from 'path'
 import test from 'ava'
 import execaHelper from '..'
 
 const withCli = execaHelper('./test/cli')
 const withNoThrowCli = execaHelper('./test/cli', false)
+const withCliOptions = execaHelper('./execa-helper/test/cli')
 
 test('throws error when stderr has content', withCli, async (t, cli) => {
   await t.throwsAsync(async () => cli('--greeting', 'Hello World!'))
@@ -17,4 +19,16 @@ test(`doesn't throw error when configured`, withNoThrowCli, async (t, cli) => {
   const { stdout, stderr } = await cli('--greeting', 'Hello World!')
   t.snapshot(stdout)
   t.snapshot(stderr)
+})
+
+test('with options', withCliOptions, async (t, cli) => {
+  const options = { cwd: resolve('..') }
+  const { stdout } = await cli(
+    '--from',
+    'Ian',
+    '--greeting',
+    'Hello World!',
+    options
+  )
+  t.is(stdout, '[Ian]: Hello World!')
 })
